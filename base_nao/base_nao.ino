78525385e7645781f3e2752fb032313b6eeeaf63
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-06-13 14:38:29 +0900

    나노 보드 기능 추가
    - 나노 데이터 수신 시 LED 표시 추가

--- base_nao/base_nao.ino
+++ base_nao/base_nao.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #include "Servo.h"
 
-SoftwareSerial XbeeSerial(4,5);   // RX, TX
+SoftwareSerial XbeeSerial(4,5);    // RX, TX
 Servo servoObj; //서보객체
 int servoPin = 12;                 // 모터 Pin
 
@@ -11,44 +11,41 @@ int servoPin = 12;                 // 모터 Pin
 void motorRun(char msg){
   /*전등 켜기 설정*/
   if(msg=='W'){
-    servoObj.write(0);            //각도 60도로 움직임
+    servoObj.write(0);              //각도 0도로 움직임
   }
 
   /*전등 끄기 설정*/
   if(msg=='X'){
-    servoObj.write(35);             //각도 0도로 움직임
+    servoObj.write(38);             //각도 38도로 움직임
   }
 }
 
 /**
- * 신호 변화 표시 LED 함수 (BLE = 1, IR = 2, Zigbee = 3)
+ * LED 블링크 함수
  */
-void ledBlink(int type){
-  for(int i=0; i<type; i=i+1){
-    digitalWrite(13, HIGH);
-    delay(200);
-    digitalWrite(13, LOW);
-    delay(200);
-  }
+void blinkLED(int delayValue){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayValue);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void setup() {
   XbeeSerial.begin(9600);
-  XbeeSerial.listen();              // SoftwareSerial에서 수신 시작
-  servoObj.attach(servoPin);        //서보 시그널 핀설정
-  servoObj.write(35);                //서보 초기각도 0도 설정
-  pinMode(13, OUTPUT);              // 신호 수신LED
+  XbeeSerial.listen();               // SoftwareSerial에서 수신 시작
+  servoObj.attach(servoPin);         //서보 시그널 핀설정
+  servoObj.write(38);                //서보 초기각도 0도 설정
+  pinMode(LED_BUILTIN, OUTPUT);      // 신호 수신LED
 }
 
 void loop() {
-  XbeeSerial.listen();      // SoftwareSerial에서 수신 시작
+  XbeeSerial.listen();              // SoftwareSerial에서 수신 시작
 
   /*소프트웨어 시리얼로 수신*/
   if(XbeeSerial.available()){
     char receive_msg = XbeeSerial.read();
+    blinkLED(100);
     if(receive_msg == 87 || receive_msg == 88){
-      motorRun(receive_msg);  // 시리얼 전송에 따른 동작 함수
-      ledBlink(3);            // 수신 여부 표시
+      motorRun(receive_msg);          // 시리얼 전송에 따른 동작 함수
     }
   }
 }
